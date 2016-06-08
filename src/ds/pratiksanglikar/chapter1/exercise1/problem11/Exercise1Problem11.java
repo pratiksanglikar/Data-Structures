d4dd0a199b6dd263ae15aaee541e5ecec1936c8b
Pratik Sanglikar <pratik.sanglikar@sjsu.edu>
2016-06-08 15:29:05 +0530

    Chapter 1, Exercise 1, Problem 9,11 solved.

---
+++ src/ds/pratiksanglikar/chapter1/exercise1/problem11/Exercise1Problem11.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package ds.pratiksanglikar.chapter1.exercise1.problem11;
+
+import ds.pratiksanglikar.lib.StdOut;
+import ds.pratiksanglikar.lib.StdRandom;
+
+/**
+ * Chapter 1, Exercise 1, Problem 11
+ * 
+ * Write a code fragment that prints the contents of a two-dimensional boolean
+ * array, using * to represent true and a space to represent false.
+ *
+ * @author Pratik Sanglikar
+ */
+public class Exercise1Problem11 {
+
+	private static boolean[][] twoDArray = new boolean[5][5];
+
+	/**
+	 * Main entry point of the program.
+	 * @param args command line arguments.
+	 */
+	public static void main(String[] args) {
+		initialize2DArray();
+		print2DArray();
+	}
+
+	/**
+	 * prints the two dimensional boolean array.
+	 *  '*' represents 'true' value.
+	 *  ' ' represents 'false' value.
+	 */
+	private static void print2DArray() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				if(twoDArray[i][j]) {
+					StdOut.print("\t*");
+				} else {
+					StdOut.print("\t ");
+				}
+			}
+			StdOut.println();
+		}
+	}
+
+	/**
+	 * initializes the two dimensional boolean array with random 
+	 * 'true' and 'false' values.
+	 */
+	private static void initialize2DArray() {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				int randomNumber = StdRandom.uniform(0, 5);
+				if(randomNumber % 2 == 0) {
+					twoDArray[i][j] = true;
+				} else {
+					twoDArray[i][j] = false;
+				}
+			}
+		}
+	}
+}
---
+++ src/ds/pratiksanglikar/chapter1/exercise1/problem9/Exercise1Problem9.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package ds.pratiksanglikar.chapter1.exercise1.problem9;
+
+/**
+ * Chapter 1, Exercise 1, Problem 9
+ * Write a code fragment that puts the binary representation of a positive integer N 
+ * into a String s. 
+ * @author Pratik Sanglikar
+ */
+public class Exercise1Problem9 {
+
+	/**
+	 * Main entry point of the program.
+	 * Accepts one command line arguments.
+	 * @param args command line arguments.
+	 */
+	public static void main(String[] args) {
+		if(args.length != 1) {
+			throw new RuntimeException("Number of command line arguments accepted: 1, got: " + args.length);
+		}
+		int number = Integer.parseInt(args[0]);
+		System.out.println(convertToBinaryString(number));
+		// Alternate approach - Interger.toBinaryString(number);
+	}
+
+	/**
+	 * function that converts the positive integer number into it's binary string 
+	 * representation
+	 * @param number to be converted.
+	 * @return converted binary string representation of the number.
+	 */
+	private static String convertToBinaryString(int number) {
+		StringBuilder binaryString = new StringBuilder("");
+		for(int n = number; n > 0; n /= 2) {
+			binaryString.insert(0, n % 2); // insert the remainder at the beginning of the string.
+		}
+		// Alternative approach, use PushDownStackLL<Integer>
+		return binaryString.toString();
+	}
+}
